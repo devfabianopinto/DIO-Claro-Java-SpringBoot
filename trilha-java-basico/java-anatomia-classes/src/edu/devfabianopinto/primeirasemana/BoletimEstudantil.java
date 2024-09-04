f2539bb924c0aa10caeb91255e9862fdcb4f9110
devfabianopinto <devfabianopinto@gmail.com>
2024-09-04 19:21:46 -0400

    atualizacao 2 repos locais claro-java

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/devfabianopinto/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,19 @@
+package edu.devfabianopinto.primeirasemana;
+
+public class BoletimEstudantil {
+
+    public static void main (String [] args) {
+
+        int mediaFinal = 7;
+
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+
+        else if(mediaFinal == 6)
+            System.out.println("Prova Final");
+        else
+            System.out.println("Aprovado");
+
+    }
+
+}
---
+++ trilha-java-basico/java-anatomia-classes/src/edu/devfabianopinto/segundasemana/MinhaClasse.java
@@ -0,0 +1,34 @@
+package edu.devfabianopinto.segundasemana;
+
+//bloco geral de codigo
+public class MinhaClasse {
+
+//metodo executavel
+    public static void main (String [] args) {
+
+        //System.out.print("Hello World!!!!");
+
+        String meuNome = "Fabiano";
+        int anoFabricacao = 2022;
+        boolean verdadeira = false;
+
+        String priNome = "Fabiano";
+        String segNome = "Fernandes";
+
+        String tstMetodo2 = nomeCompleto(priNome, segNome);
+
+        System.out.println(tstMetodo2);
+    }
+
+
+    //metodo 2
+    public static String nomeCompleto (String priNome, String segNome){
+
+//           return priNome.concat(" ").concat(segNome);
+             return "Resultado do método " + priNome.concat(" ").concat(segNome);
+    }
+
+
+
+
+}
